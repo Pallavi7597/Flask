be7d5fcfebe0211f49e15a3dabd2d50b27d56318
Pallavi7597 <66312599+Pallavi7597@users.noreply.github.com>
2023-08-11 08:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+# Object to map country codes to currency symbols
+currency_symbols = {
+    'USA': '$',
+    # Add more country codes and currency symbols here
+}
+
+# Placeholder shipment cost calculation function
+# Replace this with your actual shipping cost calculation logic
+def calculate_shipment_cost(country, weight, distance, shipment_value):
+    base_cost_per_km = 0.1
+    base_cost_per_kg = 1
+    base_cost = 10
+
+    shipment_cost = base_cost + (distance * base_cost_per_km) + (weight * base_cost_per_kg) + (shipment_value * 0.05)
+    return shipment_cost
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/calculate_shipping', methods=['POST'])
-def calculate_shipping():
-    weight = float(request.form['weight'])
-    distance = float(request.form['distance'])
-    shipment_value = float(request.form['shipmentValue'])
+@app.route('/calculate_shipment_cost', methods=['POST'])
+def calculate_shipment_cost_api():
+    data = request.get_json()
 
-    # Calculate the shipping cost using the formula
-    shipping_cost = (weight * 2) + (distance * 1) + (shipment_value * 0.01)
+    country = data.get('country', 'USA')
+    weight = data.get('weight', 0)
+    distance = data.get('distance', 0)
+    shipment_value = data.get('shipmentValue', 0)
 
-    return {
-        'shipping_cost': shipping_cost
+    currency_symbol = currency_symbols.get(country, '')
+
+    shipment_cost = calculate_shipment_cost(country, weight, distance, shipment_value)
+
+    result = {
+        'country': country,
+        'weight': weight,
+        'distance': distance,
+        'shipmentValue': shipment_value,
+        'currencySymbol': currency_symbol,
+        'shipmentCost': shipment_cost
     }
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
